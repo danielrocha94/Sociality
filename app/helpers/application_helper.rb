1c598d5de6c27e05ad611a470edeee1bf7ff5a72
danielrocha94 <danielrocha94@msn.com>
2016-04-02 23:54:49 -0600

    header and static pages added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Sociality"
+    if !page_title.empty?
+      base_title = page_title + " | " + base_title
+    end
+  end
+
 end
